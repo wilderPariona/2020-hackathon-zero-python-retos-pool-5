44d4c445e9809adbcf0ca3844c5c84bd3615fd74
wilderPariona <jhon.pariona.27@unsch.edu.pe>
2020-05-16 12:10:49 -0500

    🚀 Kata3

--- src/kata3/bot.py
+++ src/kata3/bot.py
@@ -12,20 +12,21 @@ logger = logging.getLogger(__name__)
 # Definimos algunas funciones para los comandos. Estos generalmente toman los dos argumentos update y context
 def start(update, context):
     """Envia un mensaje cuando se emita el comando /start."""
-    return ""
+    return update.message.reply_text('Hola, Geeks!')
 
 def help(update, context):
     """Envia un mensaje cuando se emita el comando /help."""
-    return ""
+    return update.message.reply_text('Ayudame!')
 
 def mayus(update, context):
-        #
-        return ""
+        msg = str(context.args[0])
+        return update.message.reply_text(msg.upper())
 
 def alreves(update, context):
-    """Repite el mensaje del usuario."""
-    #
-    return ""
+    txt_input = update.message.text
+    if(txt_input):
+        txt_al_reves = txt_input[::-1]
+        return update.message.reply_text(txt_al_reves)
 
 def error(update, context):
     """Envia los errores por consola"""
@@ -34,19 +35,19 @@ def error(update, context):
 def main():
     """Inicio del Bot"""
     #Colocamos el Token creado por FatherBot
-    updater = Updater("", use_context=True)
+    updater = Updater("1222471829:AAHmfWTZ5CGZPchfQvAw-NxWGy3IyZ1OizY", use_context=True)
 
     # Es el Registro de Comandos a través del dispartcher
-    dp = #
+    dp = updater.dispatcher#
 
     # Añadimos a la lista de Registro todos los comandos con su función [start - help - mayus]
-    #
-    #
-    #
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("mayus", mayus))
 
     # Este comando es un Trigger que se lanza cuando no hay comandos [alreves]
-    #
-    
+    dp.add_handler(MessageHandler(Filters.text, alreves))
+
     # Y este espera al error
     dp.add_error_handler(error)
 
