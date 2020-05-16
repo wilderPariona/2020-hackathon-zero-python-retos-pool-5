de5cf322f6ab45455b8be46438d587093b2a5e47
wilderPariona <jhon.pariona.27@unsch.edu.pe>
2020-05-16 11:17:54 -0500

    🚀 Kata2

--- src/kata2/rpg.py
+++ src/kata2/rpg.py
@@ -6,8 +6,12 @@ import string
 def RandomPasswordGenerator(passLen=10):
     #
     #
-    
+    txt_aleatorio = string.ascii_letters + string.digits + string.punctuation
+    clave_generada = ""
+    for i in range(passLen):
+        clave_generada += str(random.choice(txt_aleatorio))
     #
     #
+    return clave_generada
 
-    return ""
+RandomPasswordGenerator()
